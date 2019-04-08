06641a6dea6aba7821d2ef8673e313f1896ed812
bmcmahen <ben.mcmahen@gmail.com>
2019-04-08 15:40:43 -0700

    use latest sancho

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,16 @@ import Prism from "prismjs";
 import {
   Text,
   Button,
-  theme,
+  useTheme,
   Container,
   Navbar,
   Toolbar,
   IconButton,
   Tooltip,
   Layer,
-  Link
+  Link,
+  IconArrowRight,
+  DarkMode
 } from "sancho";
 import toaster from "toasted-notes";
 import { PositionsType } from "toasted-notes/lib/Message";
@@ -26,6 +28,7 @@ const NarrowContainer = ({ children }: { children: React.ReactNode }) => (
 interface AppProps {}
 
 export function App({  }: AppProps) {
+  const theme = useTheme();
   React.useEffect(() => {
     Prism.highlightAll();
   }, []);
@@ -116,7 +119,7 @@ export function App({  }: AppProps) {
           'pre[class*="language-"]': {
             borderRadius: 0,
             // boxShadow: `0 0 1px hsla(210,10.3%,22.7%,0.1), 0 0 1px 1px hsla(210,10.3%,22.7%,0.12)`,
-            [theme.breakpoints.md]: {
+            [theme.mediaQueries.md]: {
               borderRadius: theme.radii.md
             }
           },
@@ -136,41 +139,44 @@ export function App({  }: AppProps) {
           boxShadow: "none !important",
           background: "none !important",
           width: "100%",
-          zIndex: theme.zIndex.fixed
+          zIndex: theme.zIndices.fixed
         }}
       >
         <Toolbar>
-          <Text variant="h5" css={{ color: "white !important" }} gutter={false}>
-            Toasted Notes
-          </Text>
+          <DarkMode>
+            <Text variant="h5" gutter={false}>
+              Toasted Notes
+            </Text>
+          </DarkMode>
           <div css={{ marginLeft: "auto" }}>
-            <Tooltip content="View on Github">
-              <IconButton
-                variant="ghost"
-                component="a"
-                css={{ color: "white", marginLeft: theme.spaces.md }}
-                color={"white"}
-                href="https://github.com/bmcmahen/toasted-notes"
-                icon={
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 1024 1024"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"
-                      transform="scale(64)"
-                      fill="white"
-                    />
-                  </svg>
-                }
-                label="View on github"
-              />
-            </Tooltip>
+            <DarkMode>
+              <Tooltip content="View on Github">
+                <IconButton
+                  variant="ghost"
+                  component="a"
+                  css={{ marginLeft: theme.spaces.md }}
+                  href="https://github.com/bmcmahen/toasted-notes"
+                  icon={
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 1024 1024"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"
+                        transform="scale(64)"
+                        fill="white"
+                      />
+                    </svg>
+                  }
+                  label="View on github"
+                />
+              </Tooltip>
+            </DarkMode>
           </div>
         </Toolbar>
       </Navbar>
@@ -193,32 +199,32 @@ export function App({  }: AppProps) {
 
               paddingTop: `calc(${theme.spaces.lg} + 50px)`,
               paddingBottom: theme.spaces.xl,
-              [theme.breakpoints.md]: {
+              [theme.mediaQueries.md]: {
                 paddingTop: "calc(4rem + 50px)",
                 paddingBottom: 0
               }
             }}
           >
-            <Text css={{ color: "white !important" }} variant="display2">
-              Easy, flexible toast notifications for React
-            </Text>
-            <Text
-              css={{ color: "white !important", maxWidth: "38rem" }}
-              variant="lead"
-            >
-              Toasted notes provides completely configurable toast notifications
-              for your React application and it uses an imperative API which
-              makes it easy to invoke within error or event handlers.
-            </Text>
-            <Button
-              intent="primary"
-              css={{ marginTop: theme.spaces.lg }}
-              size="lg"
-              iconAfter="arrow-right"
-              onClick={play}
-            >
-              View the demo
-            </Button>
+            <DarkMode>
+              <Text variant="display2">
+                Easy, flexible toast notifications for React
+              </Text>
+              <Text css={{ maxWidth: "38rem" }} variant="lead">
+                Toasted notes provides completely configurable toast
+                notifications for your React application and it uses an
+                imperative API which makes it easy to invoke within error or
+                event handlers.
+              </Text>
+              <Button
+                intent="primary"
+                css={{ marginTop: theme.spaces.lg }}
+                size="lg"
+                iconAfter={<IconArrowRight />}
+                onClick={play}
+              >
+                View the demo
+              </Button>
+            </DarkMode>
           </div>
         </Container>
         <Divider />
@@ -234,7 +240,7 @@ export function App({  }: AppProps) {
               css={{
                 paddingTop: theme.spaces.xl,
                 paddingBottom: theme.spaces.xl,
-                [theme.breakpoints.md]: {
+                [theme.mediaQueries.md]: {
                   // paddingTop: 0,
                   paddingBottom: "3rem"
                 }
@@ -273,7 +279,7 @@ toast.notify('Hello world!')
           <div
             css={{
               marginBottom: theme.spaces.lg,
-              [theme.breakpoints.md]: {
+              [theme.mediaQueries.md]: {
                 // marginBottom: theme.spaces.xl
               }
             }}
@@ -419,13 +425,13 @@ toaster.notify(({ onClose }) => (
               css={{ marginBottom: theme.spaces.md }}
               size="lg"
               intent="primary"
-              iconAfter="arrow-right"
+              iconAfter={<IconArrowRight />}
             >
               Learn more on Github
             </Button>
             <br />
             <br />
-            <Text muted css={{ fontSize: theme.sizes[0] }}>
+            <Text muted css={{ fontSize: theme.fontSizes[0] }}>
               Created by <Link href="http://benmcmahen.com">Ben McMahen</Link>
             </Text>
           </div>
@@ -443,6 +449,7 @@ interface SectionProps {
 }
 
 function Section({ subtitle, code, title, example }: SectionProps) {
+  const theme = useTheme();
   return (
     <Layer
       css={{
@@ -453,7 +460,7 @@ function Section({ subtitle, code, title, example }: SectionProps) {
         marginRight: "-1rem",
         boxShadow: "none",
         marginBottom: theme.spaces.lg,
-        [theme.breakpoints.md]: {
+        [theme.mediaQueries.md]: {
           marginLeft: "-1.5rem",
           marginRight: "-1.5rem",
           borderRadius: theme.radii.lg,
@@ -474,7 +481,7 @@ function Section({ subtitle, code, title, example }: SectionProps) {
           css={{
             paddingLeft: theme.spaces.md,
             overflow: "hidden",
-            [theme.breakpoints.md]: {
+            [theme.mediaQueries.md]: {
               padding: 0,
               overflow: "visible"
             }
@@ -483,7 +490,7 @@ function Section({ subtitle, code, title, example }: SectionProps) {
           <pre
             css={{
               boxShadow: "none",
-              [theme.breakpoints.md]: {
+              [theme.mediaQueries.md]: {
                 boxShadow:
                   "0 0 1px hsla(210,10.3%,22.7%,0.1), 0 0 1px 1px hsla(210,10.3%,22.7%,0.12)"
               }
@@ -510,12 +517,14 @@ function Section({ subtitle, code, title, example }: SectionProps) {
   );
 }
 
-function Divider({ fill = theme.colors.palette.gray.dark, ...other }: any) {
+function Divider({ fill, ...other }: any) {
+  const theme = useTheme();
+  let filler = fill || theme.colors.palette.gray.dark;
   return (
     <div
       css={{
         display: "none",
-        [theme.breakpoints.md]: {
+        [theme.mediaQueries.md]: {
           display: "block"
         },
         top: "calc(100% - 1px)",
@@ -529,7 +538,7 @@ function Divider({ fill = theme.colors.palette.gray.dark, ...other }: any) {
       {...other}
     >
       <svg
-        fill={fill}
+        fill={filler}
         id="bigHalfCircle"
         xmlns="http://www.w3.org/2000/svg"
         version="1.1"
